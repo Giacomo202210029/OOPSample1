b42e29dc1168f49221b10cc7239253a306bc86a6
Giacomo <giacomogazr@gmail.com>
2024-03-25 21:53:59 -0500

    feat: created relation between sales order and sales order items

---
+++ Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPSample.Sales.Domain.Model.Aggregates;
+
+public class SalesOrder(int id, int customerId)
+{
+    public int Id { get;  } = id;
+    public int CustomerId { get;  } = customerId;
+
+    private readonly List<SalesOrderItem> _items = []; 
+    
+    //el unico que alberga la logica de negocio de agregar un aggregate es un aggregate
+    public void AddItem(int productId, int quantity, double unitPrice)
+    {
+        _items.Add(new SalesOrderItem(Id, productId, quantity, unitPrice));
+    }
+}
